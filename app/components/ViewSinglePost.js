9a67f4a5747660bdceebf03c4f5079eb2e2e48df
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2020-05-22 17:29:37 -0500

    Make post page dynamic

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Page from "./Page";
+import axios from "axios";
 
 export default () => {
+  const postId = useParams().id;
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await axios.get(`/post/${postId}`);
+        console.log(response.data);
+        if (response?.data) {
+          setPost(response.data);
+          setIsLoading(false);
+        }
+      } catch (e) {
+        console.error("Something went wrong", e);
+      }
+    };
+    fetchPost();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Page title="...">
+        <div>Loading...</div>
+      </Page>
+    );
+  }
+
+  const date = new Date(post.createdDate);
+  const formattedDate = `${
+    date.getMonth() + 1
+  }/${date.getDay()}/${date.getFullYear()}`;
+
   return (
-    <Page title="Post">
+    <Page title={post.title}>
       <div className="d-flex justify-content-between">
-        <h2>Example Post Title</h2>
+        <h2>{post.title}</h2>
         <span className="pt-2">
           <a href="#" className="text-primary mr-2" title="Edit">
             <i className="fas fa-edit"></i>
@@ -18,30 +54,12 @@ export default () => {
 
       <p className="text-muted small mb-4">
         <a href="#">
-          <img
-            className="avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />
+          <img className="avatar-tiny" src={post.author.avatar} />
         </a>
-        Posted by <a href="#">brad</a> on 2/10/2020
+        Posted by <a href="#">{post.author.username}</a> on {formattedDate}
       </p>
 
-      <div className="body-content">
-        <p>
-          Lorem ipsum dolor sit <strong>example</strong> post adipisicing elit.
-          Iure ea at esse, tempore qui possimus soluta impedit natus voluptate,
-          sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat
-          asperiores at.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod
-          asperiores corrupti omnis qui, placeat neque modi, dignissimos, ab
-          exercitationem eligendi culpa explicabo nulla tempora rem? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Iure ea at esse, tempore
-          qui possimus soluta impedit natus voluptate, sapiente saepe modi est
-          pariatur. Aut voluptatibus aspernatur fugiat asperiores at.
-        </p>
-      </div>
+      <div className="body-content">{post.body}</div>
     </Page>
   );
 };
