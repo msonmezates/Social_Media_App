cf0cdf9a32479c4682b3778ba16ffc060019e33a
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2020-06-14 12:54:22 -0500

    Create a state when user enters input in search bar

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,17 +1,46 @@
 import React, { useContext, useEffect } from "react";
+import { useImmer } from "use-immer";
 import DispatchContext from "../DispatchContext";
+import axios from "axios";
 
 export default () => {
   const appDispatch = useContext(DispatchContext);
 
+  const [state, setState] = useImmer({
+    searchTerm: "",
+    results: [],
+    show: "neither",
+    requestCount: 0
+  });
+
+  // Handle keyboard events
   useEffect(() => {
     document.addEventListener("keyup", searchKeyPressHandler);
-
     // Cleanup
     return () => {
       document.removeEventListener("keyup", searchKeyPressHandler);
     };
   }, []);
+
+  // Handle search
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setState(draft => {
+        draft.requestCount++;
+      });
+    }, 3000);
+
+    // Cleanup to handle api call
+    // Don't make api call for every single character user types
+    return () => clearTimeout(delay);
+  }, [state.searchTerm]);
+
+  // Handle api call based on count
+  useEffect(() => {
+    if (state.requestCount) {
+      // make api call
+    }
+  }, [state.requestCount]);
 
   const searchKeyPressHandler = e => {
     if (e.code === "Escape" || e.keyCode === 27) {
@@ -21,6 +50,13 @@ export default () => {
 
   const handleCloseIcon = () => {
     appDispatch({ type: "closeSearch" });
+  };
+
+  const handleInputSearch = e => {
+    const value = e?.target?.value;
+    setState(draft => {
+      draft.searchTerm = value;
+    });
   };
 
   return (
@@ -37,6 +73,7 @@ export default () => {
             id="live-search-field"
             className="live-search-field"
             placeholder="What are you interested in?"
+            onChange={handleInputSearch}
           />
           <span className="close-live-search" onClick={handleCloseIcon}>
             <i className="fas fa-times-circle"></i>
@@ -49,7 +86,7 @@ export default () => {
           <div className="live-search-results live-search-results--visible">
             <div className="list-group shadow-sm">
               <div className="list-group-item active">
-                <strong>Search Results</strong> (3 items found)
+                <strong>Search Results</strong> (30 items found)
               </div>
               <a href="#" className="list-group-item list-group-item-action">
                 <img
@@ -57,24 +94,6 @@ export default () => {
                   src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
                 />{" "}
                 <strong>Example Post #1</strong>
-                <span className="text-muted small">by brad on 2/10/2020 </span>
-              </a>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img
-                  className="avatar-tiny"
-                  src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
-                />{" "}
-                <strong>Example Post #2</strong>
-                <span className="text-muted small">
-                  by barksalot on 2/10/2020{" "}
-                </span>
-              </a>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img
-                  className="avatar-tiny"
-                  src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-                />{" "}
-                <strong>Example Post #3</strong>
                 <span className="text-muted small">by brad on 2/10/2020 </span>
               </a>
             </div>
