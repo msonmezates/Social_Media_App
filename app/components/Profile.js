f6845c7e265928840cb1e92f124045b5ac405e98
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2020-06-20 21:03:27 -0500

    Add follow unfollow logic

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -14,8 +14,8 @@ export default () => {
   const { username } = useParams();
 
   const [state, setState] = useImmer({
-    followActionLoadingFalse: false,
-    startFollowingRequestcount: 0,
+    followActionLoading: false,
+    startFollowingRequestCount: 0,
     stopFollowingRequestCount: 0,
     profileData: {
       profileUsername: "",
@@ -61,17 +61,137 @@ export default () => {
     };
   }, []);
 
+  // Handle start following api call
+  useEffect(() => {
+    if (state.startFollowingRequestCount) {
+      // Disable follow button until getting response from api
+      setState(draft => {
+        draft.followActionLoading = true;
+      });
+
+      // Create cancel token
+      const ourRequest = axios.CancelToken.source();
+
+      // useEffect can't take async directly
+      const fetchData = async () => {
+        try {
+          const response = await axios.post(
+            `/addFollow/${state.profileData.profileUsername}`,
+            {
+              token
+            },
+            { cancelToken: ourRequest.token }
+          );
+          if (response?.data) {
+            setState(draft => {
+              draft.profileData.isFollowing = true;
+              draft.profileData.counts.followerCount++;
+              draft.followActionLoading = false;
+            });
+          }
+        } catch (e) {
+          console.error(
+            "There was an error or network request was cancelled",
+            e
+          );
+        }
+      };
+
+      fetchData();
+
+      // Cleanup function in case network request is cancelled
+      return () => {
+        ourRequest.cancel();
+      };
+    }
+  }, [state.startFollowingRequestCount]);
+
+  // Handle stop following api call
+  useEffect(() => {
+    if (state.stopFollowingRequestCount) {
+      // Disable follow button until getting response from api
+      setState(draft => {
+        draft.followActionLoading = true;
+      });
+
+      // Create cancel token
+      const ourRequest = axios.CancelToken.source();
+
+      // useEffect can't take async directly
+      const fetchData = async () => {
+        try {
+          const response = await axios.post(
+            `/removeFollow/${state.profileData.profileUsername}`,
+            {
+              token
+            },
+            { cancelToken: ourRequest.token }
+          );
+          if (response?.data) {
+            setState(draft => {
+              draft.profileData.isFollowing = false;
+              draft.profileData.counts.followerCount--;
+              draft.followActionLoading = false;
+            });
+          }
+        } catch (e) {
+          console.error(
+            "There was an error or network request was cancelled",
+            e
+          );
+        }
+      };
+
+      fetchData();
+
+      // Cleanup function in case network request is cancelled
+      return () => {
+        ourRequest.cancel();
+      };
+    }
+  }, [state.stopFollowingRequestCount]);
+
+  const handleStartFollowing = () => {
+    setState(draft => {
+      draft.startFollowingRequestCount++;
+    });
+  };
+
+  const handleStopFollowing = () => {
+    setState(draft => {
+      draft.stopFollowingRequestCount++;
+    });
+  };
+
   return (
     <Page title="Profile">
       <h2>
         <img className="avatar-small" src={state.profileData.profileAvatar} />
         {state.profileData.profileUsername}
-        <button
-          className="btn btn-primary btn-sm ml-2"
-          onClick={() => console.log("following")}
-        >
-          Follow <i className="fas fa-user-plus"></i>
-        </button>
+        {appState.isLoggedIn &&
+          !state.profileData.isFollowing &&
+          state.profileData.profileUsername &&
+          state.profileData.profileUsername !== appState.user.username && (
+            <button
+              className="btn btn-primary btn-sm ml-2"
+              onClick={handleStartFollowing}
+              disabled={state.followActionLoading}
+            >
+              Follow <i className="fas fa-user-plus"></i>
+            </button>
+          )}
+        {appState.isLoggedIn &&
+          state.profileData.isFollowing &&
+          state.profileData.profileUsername &&
+          state.profileData.profileUsername !== appState.user.username && (
+            <button
+              className="btn btn-danger btn-sm ml-2"
+              onClick={handleStopFollowing}
+              disabled={state.followActionLoading}
+            >
+              Stop Following <i className="fas fa-user-times"></i>
+            </button>
+          )}
       </h2>
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
